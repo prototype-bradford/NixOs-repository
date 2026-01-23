8d4a947aecbe6c4892b3c035377a44da6535f8fb
bradford <lucasdaniel.prtype@proton.me>
2026-01-23 16:37:21 -0300

    add marketplace in spicetify

--- configuration.nix
+++ configuration.nix
@@ -169,6 +169,10 @@
     ];
 
     theme = inputs.spicetify.legacyPackages.${pkgs.system}.themes.hazy;
+
+    enabledCustomApps = [
+      inputs.spicetify.legacyPackages.${pkgs.system}.apps.marketplace
+    ];
   };
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
